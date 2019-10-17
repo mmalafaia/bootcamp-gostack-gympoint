d43def0edbeab5650cc1776334f234413e9ec38f
mmalafaia <mmalafaia@gmail.com>
2019-10-17 15:19:13 -0300

    Students working

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -8,20 +8,22 @@ class StudentController {
       email: Yup.string()
         .email()
         .required(),
-      age: Yup.integer().required(),
-      weight: Yup.integer().required(),
-      height: Yup.integer().required(),
+      age: Yup.number()
+        .integer()
+        .required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const StudentExists = await Student.findOne({
+    const studentExists = await Student.findOne({
       where: { email: req.body.email },
     });
 
-    if (StudentExists) {
+    if (studentExists) {
       return res.status(400).json({ error: 'User alredy exists.' });
     }
 
@@ -43,9 +45,9 @@ class StudentController {
     const schema = Yup.object().shape({
       name: Yup.string(),
       email: Yup.string().email(),
-      age: Yup.integer(),
-      weight: Yup.integer(),
-      height: Yup.integer(),
+      age: Yup.number().integer(),
+      weight: Yup.number(),
+      height: Yup.number(),
     });
 
     if (!(await schema.isValid(req.body))) {
