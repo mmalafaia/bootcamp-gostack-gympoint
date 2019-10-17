fd9d69680d5453398be80d68206551f20aec0f41
mmalafaia <mmalafaia@gmail.com>
2019-10-17 14:12:15 -0300

    Erro no Sessions Store

---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+// The MD5 hash for gobarberoutnex2020 is : a3c68398dfef9d9ec0764929ca8e4331
+
+export default {
+  secret: 'a3c68398dfef9d9ec0764929ca8e4331',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.post('/students', StudentController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.put('/students', StudentController.update);
 
 export default routes;
