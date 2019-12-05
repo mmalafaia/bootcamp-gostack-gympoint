c074ce2d28ffdec86a9fe4b7bb680c9e9f980c64
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-05 16:59:44 -0300

    Commit: Add cors, youch, sentry and .env config

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,4 @@
-// The MD5 hash for gobarberoutnex2020 is : a3c68398dfef9d9ec0764929ca8e4331
-
 export default {
-  secret: 'a3c68398dfef9d9ec0764929ca8e4331',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
